f843b0f530ff10e2b43f7f045056be191791e347
Kyle Chinick <kylechinick@icloud.com>
2022-06-03 13:52:57 -0700

    Add reset() call to end of form-submission function to reset all form values

--- js/scripts.js
+++ js/scripts.js
@@ -10,37 +10,40 @@ function quizAnalyzer(
 
   if (colorParam === 'red' || colorParam === 'yellow') {
     const quizResult = 'You should learn Javascript';
+    console.log('You should learn Javascript');
     $('#language-quiz-result-container').text(quizResult);
   } else if (colorParam === 'blue' || colorParam === 'green') {
     const quizResult = 'You should learn C#';
+    console.log('You should learn C#');
     $('#language-quiz-result-container').text(quizResult);
   } else {
     const quizResult = 'You should learn Python';
+    console.log('You should learn Python');
     $('#language-quiz-result-container').text(quizResult);
   }
 }
 
 $(document).ready(function () {
-  $('#languagesQuizForm').submit(function (event) {
+  $('#languages-quiz-form').submit(function (event) {
     event.preventDefault();
-    console.log('Form submitted!');
+    // console.log('Form submitted!');
 
     const colorResponse = $('#colorQuestion').val();
-    console.log(colorResponse);
+    // console.log(colorResponse);
 
     const foodResponse = $('input:radio[name=foodQuestion]:checked').val();
-    console.log(foodResponse);
+    // console.log(foodResponse);
 
     const rollerbladesResponse = $(
       'input:radio[name=rollerbladesQuestion]:checked'
     ).val();
-    console.log(rollerbladesResponse);
+    // console.log(rollerbladesResponse);
 
     const llamasResponse = parseInt($('#llamasQuestion').val());
-    console.log(llamasResponse);
+    // console.log(llamasResponse);
 
     const alienNameResponse = $('#alienNameQuestion').val();
-    console.log(alienNameResponse);
+    // console.log(alienNameResponse);
 
     quizAnalyzer(
       colorResponse,
@@ -49,5 +52,6 @@ $(document).ready(function () {
       llamasResponse,
       alienNameResponse
     );
+    document.getElementById('languages-quiz-form').reset();
   });
 });
