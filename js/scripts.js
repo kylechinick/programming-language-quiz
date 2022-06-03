a065b23fb08c250c80d74a017846165221462ff3
Kyle Chinick <kylechinick@icloud.com>
2022-06-03 10:29:15 -0700

    Build input retrevial calls with console.log tests for each form field

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  console.log('Test');
+  $('#languagesQuizForm').submit(function (event) {
+    event.preventDefault();
+    console.log('Form submitted!');
+
+    const colorResponse = $('#colorQuestion').val();
+    console.log(colorResponse);
+
+    const foodResponse = $('input:radio[name=foodQuestion]:checked').val();
+    console.log(foodResponse);
+
+    const rollerbladesResponse = $(
+      'input:radio[name=rollerbladesQuestion]:checked'
+    ).val();
+    console.log(rollerbladesResponse);
+
+    const llamasResponse = parseInt($('#llamasQuestion').val());
+    console.log(llamasResponse);
+
+    const alienNameResponse = $('#alienNameQuestion').val();
+    console.log(alienNameResponse);
+  });
 });
