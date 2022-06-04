e5b5fede0a79952fd2da545d5a23cc33cc635c24
Kyle Chinick <kylechinick@icloud.com>
2022-06-03 17:08:55 -0700

    Remove testing console.logs

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,17 @@ function quizAnalyzer(
   llamasParam,
   alienNameParam
 ) {
-  const allArgumentsTestVar = `You selected: ${colorParam.toString()}, ${foodParam.toString()}, ${rollerbladesParam.toString()}, ${llamasParam.toString()}, and ${alienNameParam.toString()}.`;
-  console.log(allArgumentsTestVar);
-
   if (colorParam === 'red' || colorParam === 'yellow') {
     const quizResult = 'You should learn Javascript';
-    console.log('You should learn Javascript');
     $('#language-quiz-result-announcement').text(quizResult);
   } else if (colorParam === 'blue' || colorParam === 'green') {
     const quizResult = 'You should learn C#';
-    console.log('You should learn C#');
     $('#language-quiz-result-announcement').text(quizResult);
   } else if (colorParam === 'orange' || colorParam === 'purple') {
     const quizResult = 'You should learn Python';
-    console.log('You should learn Python');
     $('#language-quiz-result-announcement').text(quizResult);
   } else {
-    console.log(
+    alert(
       'Something went wrong, please reload this webpage and submit your responses again.'
     );
   }
@@ -34,24 +28,18 @@ function showLanguageQuizResultContainer() {
 $(document).ready(function () {
   $('#languages-quiz-form').submit(function (event) {
     event.preventDefault();
-    // console.log('Form submitted!');
 
     const colorResponse = $('#colorQuestion').val();
-    // console.log(colorResponse);
 
     const foodResponse = $('input:radio[name=foodQuestion]:checked').val();
-    // console.log(foodResponse);
 
     const rollerbladesResponse = $(
       'input:radio[name=rollerbladesQuestion]:checked'
     ).val();
-    // console.log(rollerbladesResponse);
 
     const llamasResponse = parseInt($('#llamasQuestion').val());
-    // console.log(llamasResponse);
 
     const alienNameResponse = $('#alienNameQuestion').val();
-    // console.log(alienNameResponse);
 
     quizAnalyzer(
       colorResponse,
